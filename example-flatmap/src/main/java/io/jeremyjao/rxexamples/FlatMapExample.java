54a962e127492e84f38639213c67ee3f36cc8ab4
Michael Antonacci <michaelantonacci2@gmail.com>
2016-08-29 00:34:00 -0400

    simplify flatmap example
    break up long call chain
    explicitly create an Observer and pass it to subscribe
    modify splitDelay method to handle splitting string *and* emiting 1 word/second

--- example-flatmap/src/main/java/io/jeremyjao/rxexamples/FlatMapExample.java
+++ example-flatmap/src/main/java/io/jeremyjao/rxexamples/FlatMapExample.java
@@ -1,47 +1,51 @@
 package io.jeremyjao.rxexamples;
 
 import rx.Observable;
-import rx.schedulers.Schedulers;
+import rx.Observer;
+import rx.observers.Observers;
 
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Flatmap example that delays iteration of the split string
  *
  * Created by epicstar on 8/28/16.
+ * @author Jeremy Jao
+ * @author Michael Antonacci
  */
 public class FlatMapExample {
-    private final static String input = "why hello there";
+    private final static String[] input = new String[] {"why hello there","how are you","doing today?"};
 
     public static void main(String[] args) {
 
+        Observable<String> phrases = Observable.from(input);
+
         //noinspection Convert2MethodRef
-        Observable.just(input)
-                .map(st -> st.split(" "))
-                .flatMap(s -> splitDelay(s))
-                .subscribeOn(Schedulers.io())
-                .observeOn(Schedulers.immediate())
-                .subscribe(
-                        result -> System.out.println("Result: " + result),
-                        error -> error.printStackTrace(),
-                        () -> {
-                            System.out.println("Complete... exiting explicitly");
-                            System.exit(0);
-                        }
-                );
-        //noinspection InfiniteLoopStatement,StatementWithEmptyBody
-        while (true) {}
+        Observable<String> words = phrases
+                .flatMap(s -> splitDelay(s));
+
+        //noinspection Convert2MethodRef
+        Observer<String> obs = Observers.create(
+            result -> System.out.println("Result: " + result),
+            error -> error.printStackTrace(),
+            () -> System.out.println("Complete... exiting explicitly")
+        );
+
+        words.subscribe(obs);
+        (new Scanner(System.in)).nextLine();
     }
 
     /**
-     * Iterates over spl every 1 second
-     * @param spl a string
-     * @return an observable that emits something in spl every 1 second
+     * split string into words and emit one word per second
+     * Don't worry about the implementation
+     * @param str a string
+     * @return an observable that emits one word per second
      */
-    private static Observable<? extends String> splitDelay(String[] spl) {
+    private static Observable<String> splitDelay(String str) {
         return Observable
-                .from(spl)
-                .zipWith(Observable.interval(1, TimeUnit.SECONDS), (r, x) -> r);
+                .from(str.split(" "))
+                .zipWith(Observable.interval(1, TimeUnit.SECONDS),(r, i) -> r);
     }
 
 }
